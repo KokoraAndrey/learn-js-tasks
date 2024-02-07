9ec5490e22e19871c24cd254dcaaef8f7e6ac7af
Kokora Andrey <andrey18121996@gmail.com>
2024-02-08 01:42:35 +0300

    tasks: 4.4 - 3

--- 4.1 - 4.8[Object.Basic]/tasks_4.4.js
+++ 4.1 - 4.8[Object.Basic]/tasks_4.4.js
@@ -18,3 +18,23 @@ let calculator = {
 calculator.read(5, 5);
 console.log(calculator.sum());
 console.log(calculator.mul());
+
+//? Цепь вызовов
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep: function () {
+        // показывает текущую ступеньку
+        alert(this.step);
+        return this;
+    },
+};
+
+ladder.up().up().down().showStep().down();
